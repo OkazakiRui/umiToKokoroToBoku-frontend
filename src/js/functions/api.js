34b211ba255fd79c9c24dd74c21f98a460c28075
RuiOkazaki <okazaki200226@gmail.com>
2022-01-16 12:57:55 +0900

    update: 例外処理を記述した

--- src/js/functions/api.js
+++ src/js/functions/api.js
@@ -1,3 +1,4 @@
+import notyf from './notyf';
 // Todo: fetch先を変更する
 
 /**
@@ -6,22 +7,19 @@
  * @returns {void}
  */
 export const sendPost = async (message) => {
-  try {
-    if (!message) throw new Error('メッセージがありません!');
-    const data = {
-      post_text: message,
-    };
-    const parms = {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    };
-    const result = await fetch('http://localhost:8080/api/posts', parms);
-    const json = await result.json();
-    console.log(json);
-  } catch (err) {
-    console.log(err);
-  }
+  if (!message) return;
+  const data = {
+    post_text: message,
+  };
+  const parms = {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch('http://localhost:8080/api/posts', parms);
+  response.status === 201
+    ? notyf.success('正常に登録されました!')
+    : notyf.error(`${response.status}: ${response.statusText}`);
 };
